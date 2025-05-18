fd64ea5c6e0c4a17a7563c5c11e708bc7d27ad7d
anasmunawar2 <anasmunawar2@gmail.com>
2025-05-18 06:14:14 +0500

    setup modified

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <View>
+      <View className="flex-1 items-center justify-center bg-white">
         <Text>Hello</Text>
       </View>
     </SafeAreaView>
