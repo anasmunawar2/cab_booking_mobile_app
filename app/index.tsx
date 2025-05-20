785a1d9857f2f229c84f3c7565202c88ea27b69a
anasmunawar2 <anasmunawar2@gmail.com>
2025-05-20 17:59:05 +0500

    assets added

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
+    <SafeAreaView>
       <Text className="text-5xl">Hello</Text>
-    </View>
+    </SafeAreaView>
   );
 }
