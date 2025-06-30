b0a230cb81d158550f5327012442f7574a6784eb
anasmunawar2 <anasmunawar2@gmail.com>
2025-06-30 18:43:57 +0500

    feat: update package dependencies for maps functionality

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,10 +1,21 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
 const Map = () => {
+  // const region = {};
   return (
-    <View>
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full rounded-2xl"
+      tintColor="black"
+      mapType="mutedStandard"
+      showsPointOfInterest={false}
+      // initialRegion={region}
+      showsUserLocation={true}
+      userInterfaceStyle="light"
+    >
       <Text>Map</Text>
-    </View>
+    </MapView>
   );
 };
 
